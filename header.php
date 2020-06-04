6e537e747d5dd6b3fa4c661873b0398c6d453465
jeremy <je.gautier137@gmail.com>
2020-06-04 10:07:14 +0200

    oups

---
+++ header.php
@@ -0,0 +1,13 @@
+<header id="testheader" class="header">
+	<nav id="menu">
+		<img class="logoImg" src="../img/enter.jpg" alt="logo">
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="planvol.php">page 1</a></li>
+			<li><a href="convertisseur.php">page2</a></li>
+			<li><a href="forum.php">page3</a></li>
+			<li><a href="stats.php">page4</a></li>
+			<li><a href="cartes.php">page5</a></li>
+		</ul>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>DIDAX</title>
+    <link href="style/style.css" rel="Stylesheet" type="text/css" >
+
+</head>
+<body id ="balise"class="baliseBlack">
+
+<?php include "header.php" ?>
+</div>
+<?php include "footer.php" ?>
+</body>
+</html>
